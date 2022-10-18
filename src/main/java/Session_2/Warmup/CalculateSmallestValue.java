9045a41cd268095fb5f6f98303d359da2860032e
shankkalra <shankkalra@gmail.com>
2022-10-17 22:28:36 -0300

    findMax

--- src/main/java/Session_2/Warmup/CalculateSmallestValue.java
+++ src/main/java/Session_2/Warmup/CalculateSmallestValue.java
@@ -1,2 +1,24 @@
-package Session_2.Warmup;public class CalculateSmallestValue {
+package Session_2.Warmup;
+
+import java.util.Scanner;
+
+public class CalculateSmallestValue {
+
+    final static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        findNumberLessThanFactorial();
+    }
+
+    private static void findNumberLessThanFactorial() {
+        long value = scanner.nextLong();
+        long fact = 1;
+        for (int i = 1 ; ; i++ ) {
+            fact = fact * i;
+            if(fact > value) {
+                System.out.println(i);
+                break;
+            }
+        }
+    }
 }
