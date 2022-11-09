6417055fc399de8ec1032900565637446409e6e2
shankkalra <shankkalra@gmail.com>
2022-11-08 20:25:07 -0400

    Method

---
+++ src/main/java/Session_2/Method/IsVowel.java
@@ -0,0 +1,17 @@
+package Session_2.Method;
+
+import java.util.Scanner;
+
+public class IsVowel {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char letter = scanner.nextLine().charAt(0);
+        System.out.println(isVowel(letter) ? "YES" : "NO");
+    }
+
+    public static boolean isVowel(char ch) {
+        // write your code here
+        String vowels = "aeiouAEIOU";
+        return vowels.indexOf(ch) != -1;
+    }
+}
