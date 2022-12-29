11c11382f21cbeb79dca981885e7e8b83e9ece4a
shankkalra <shankkalra@gmail.com>
2022-12-28 20:24:05 -0400

    Array Cehck

---
+++ src/main/java/Session_2/Errors/SquareElements.java
@@ -0,0 +1,32 @@
+package Session_2.Errors;
+
+import java.util.Scanner;
+
+public class SquareElements {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int range = scanner.nextInt();
+
+        int arr[] = new int[range];
+        for (int i = 0; i < range; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        int index = scanner.nextInt();
+
+        calculateSquare(arr,index);
+    }
+
+
+    public static void calculateSquare(int[] array, int index) {
+        // write your code here
+
+        if(array == null || array.length<0 || index > array.length-1){
+            System.out.println("Exception!");
+        }
+        else{
+            System.out.println((int)Math.pow(array[index],2));
+        }
+
+    }
+}
