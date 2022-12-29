b60de78a2328c4c3b8e21b541bb1798dcd0f9bc0
shankkalra <shankkalra@gmail.com>
2022-12-29 16:31:58 -0400

    Square

--- src/main/java/Session_2/Errors/SquareElements.java
+++ src/main/java/Session_2/Errors/SquareElements.java
@@ -21,7 +21,7 @@ public class SquareElements {
     public static void calculateSquare(int[] array, int index) {
         // write your code here
 
-        if(array == null || array.length<0 || index > array.length-1){
+        if(array == null || array.length<0 || index > array.length-1 || index < 0){
             System.out.println("Exception!");
         }
         else{
