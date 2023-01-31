40bcaf8bdc75dae9b34db0ecdb3646ee803211cc
shankkalra <shankkalra@gmail.com>
2023-01-30 20:41:18 -0400

    Lambda

---
+++ src/main/java/Revision/Collections/RemoveDuplicateElem.java
@@ -0,0 +1,25 @@
+package Revision.Collections;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class RemoveDuplicateElem {
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,1,1));
+        //LinkedHashSet
+        LinkedHashSet<Integer> linkedHashSet = new LinkedHashSet<>(numbers);
+        System.out.println(linkedHashSet);
+
+        ArrayList<Integer> numberListWithoutDup = new ArrayList<>(linkedHashSet);
+        System.out.println(numberListWithoutDup);
+
+        //Stream
+        ArrayList<Integer> numbers1 = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,1,1,1));
+        List<Integer> collect = numbers1.stream().distinct().collect(Collectors.toList());
+        System.out.println(collect);
+    }
+}
