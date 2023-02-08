2eee2aa4011223b63978764315c47e09c59756d6
shankkalra <shankkalra@gmail.com>
2023-02-08 10:33:39 -0400

    Date Prob Solved

--- src/main/java/Chuck_Norris_Cipher_Encoder/Problems/Problems/DateConversion.java
+++ src/main/java/Chuck_Norris_Cipher_Encoder/Problems/Problems/DateConversion.java
@@ -3,46 +3,16 @@ package Chuck_Norris_Cipher_Encoder.Problems.Problems;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class DateConversion {
     public static void main(String[] args) {
-        String date = "2012-09-28";
-        String [] dateArray = date.split("-");
-        String join = String.join("/", Arrays.asList(dateArray[1],dateArray[2],dateArray[0]));
+        String[] date = new Scanner(System.in).next().split("-");
+        String join = String.join("/", Arrays.asList(date[1], date[2], date[0]));
         System.out.println(join);
-
-        int month = Integer.parseInt(dateArray[1]);
-        int year = Integer.parseInt(dateArray[0]);
-        int dates = Integer.parseInt(dateArray[2]);
-
-        //String europeanDatePattern = "MM/dd/yyyy";
-        DateTimeFormatter europeanDateFormatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
-        System.out.println(europeanDateFormatter.format(LocalDate.of(year, month, dates)));
-
-
-    }
-
-    private static void extractInMMDDYYYYFormat(String[] dateArray) {
-
-        int month = Integer.parseInt(dateArray[1]);
-        int year = Integer.parseInt(dateArray[0]);
-        int date = Integer.parseInt(dateArray[2]);
-
-        if((month >1 || month <=12) && (date >1 || date<=31)  ){
-            System.out.println(month+"/"+date+"/"+year);
-        }
-
-        /*System.out.println(LocalDate.of(date).format("MM/DD/YYYY"));
-
-        String[] dateArray = date.split("-");
-        System.out.println(dateArray[1]+"/"+dateArray[2]+"/"+dateArray[0]);
-        extractInMMDDYYYYFormat(dateArray);*/
-
-        /*DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
-        String formattedDate = date.format(String.valueOf(dateTimeFormatter));
-        System.out.println(formattedDate);*/
     }
 }
+
 
 /*
 Write a program that takes a date string formatted as YYYY-MM-DD as input, then converts and outputs it as MM/DD/YYYY.
