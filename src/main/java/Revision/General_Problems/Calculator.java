d2417081cfd81111c88ee681466050535d19c6ac
shankkalra <shankkalra@gmail.com>
2023-03-16 09:54:16 -0300

    Cal

---
+++ src/main/java/Revision/General_Problems/Calculator.java
@@ -0,0 +1,49 @@
+package Revision.General_Problems;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int ans = 0;
+
+        while(true){
+            System.out.print("Enter the operator: ");
+            char op = in.next().trim().charAt(0);
+
+            if (op == '+' || op == '-' || op == '*' ||op == '/' ||op == '%') {
+                System.out.print("Enter 2 numbers: ");
+                int n1 = in.nextInt();
+                int n2 = in.nextInt();
+
+                if (op == '+') {
+                    ans = n1 + n2;
+                }
+                if (op == '-') {
+                    ans = n1 - n2;
+                }
+                if (op == '*') {
+                    ans = n1 * n2;
+                }
+                if (op == '/') {
+                    if (n2 != 0) {
+                        ans = n1 / n2;
+                    }
+                }
+                if (op == '%') {
+                    ans = n1 % n2;
+                }
+            }
+
+            else if (op == 'x' || op == 'X') {
+                    break;
+            }
+
+            else{
+                System.out.println("Invalid Operation");
+            }
+            System.out.println(ans);
+        }
+
+    }
+}
