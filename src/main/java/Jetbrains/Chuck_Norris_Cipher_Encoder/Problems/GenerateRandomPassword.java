8899936ba49507baa4bc8ed8bee5e746b32c1422
shankkalra <shankkalra@gmail.com>
2023-03-19 20:28:31 -0300

    Password Generator

--- src/main/java/Jetbrains/Chuck_Norris_Cipher_Encoder/Problems/GenerateRandomPassword.java
+++ src/main/java/Jetbrains/Chuck_Norris_Cipher_Encoder/Problems/GenerateRandomPassword.java
@@ -11,8 +11,29 @@ public class GenerateRandomPassword {
         System.out.println("How many characters long password should be ?");
         int length = in.nextInt();
 
+        String [] randomPasswords = new String[total];
+
+        //Loop through total number of passwords
+        for(int i = 0; i < total; i++){
+            //Generate one random password
+            String randomPassword = "";
+            for(int j = 0; j < length; j++){
+                //Generate one random character
+                randomPassword +=  randomCharacter();
+            }
+            //Add random Password
+            randomPasswords[i] = randomPassword;
+        }
+
+        //Print array of Passwords
+        printPasswords(randomPasswords);
     }
 
+    public static void printPasswords(String[] arr){
+        for(int i = 0; i<arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
     //0-9 => 48-57 ASCII
     //A-Z => 65-90 ASCII
     //a-z => 97-122 ASCII
@@ -24,6 +45,21 @@ public class GenerateRandomPassword {
 
         int rand = (int)(Math.random()*62);
 
-        return 0;
+        //rand is btwn 0-9 => digit => 48-57
+        if(rand<=9){
+            int ascii = rand + 48;
+            return (char)(ascii);
+        }
+        //uppercase {65-90}
+        else if(rand<=35){
+            int ascii = rand + 55; //65 -10 => 55
+            return (char)(ascii);
+        }
+        //lowercase {97-122}
+        else{
+            int ascii = rand + 61; //97 -36 => 55
+            return (char)(ascii);
+        }
+
     }
 }
---
+++ src/main/java/Jetbrains/Chuck_Norris_Cipher_Encoder/Problems/PasswordGeneratorCode.java
@@ -0,0 +1,52 @@
+package Jetbrains.Chuck_Norris_Cipher_Encoder.Problems;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public class PasswordGeneratorCode {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();//upper
+        int b = sc.nextInt();//lower
+        int c = sc.nextInt();//digits
+        int n = sc.nextInt();//length
+
+        List<Character> list = new ArrayList<Character>();
+        while (list.size() < n){
+            addUpper(a,list);
+            addLower(b,list);
+            addNumbers(c,list);
+        }
+
+        StringBuilder builder = new StringBuilder();
+
+        for(int i = 0; i < n; i++){
+            builder.append(list.toArray()[i]);
+        }
+        System.out.println(builder.toString());
+    }
+
+    public static Character createRandomSymbol() {
+        return (char) (new Random().nextInt(26) + 'a');
+    }
+
+    public static void addUpper(int a, List<Character> list) {
+        for (int i = 0; i < a; i++) {
+            list.add(Character.toUpperCase(createRandomSymbol()));
+        }
+    }
+
+    public static void addLower(int b, List<Character> list) {
+        for (int i = 0; i < b; i++) {
+            list.add(Character.toLowerCase(createRandomSymbol()));
+        }
+    }
+
+    public static void addNumbers(int c, List<Character> list) {
+        for (int i = 0; i < c; i++) {
+            list.add(Character.forDigit(new Random().nextInt(10), 10));
+        }
+    }
+}
