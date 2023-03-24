8ee99754993b5744233446000c2629001e9c2557
shankkalra <shankkalra@gmail.com>
2023-03-24 12:12:20 -0300

    CutMiddle

---
+++ src/main/java/Jetbrains/Chuck_Norris_Cipher_Encoder/Problems/Middle_Of_String.java
@@ -0,0 +1,25 @@
+package Jetbrains.Chuck_Norris_Cipher_Encoder.Problems;
+
+import java.util.Scanner;
+
+public class Middle_Of_String {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        String str = in.next();
+        System.out.println(getStringCutIn(str));
+    }
+
+    private static String getStringCutIn(String str) {
+        if(str==null | str.isEmpty()){
+            return str;
+        }
+
+        int length = str.length();
+        int index = length/2;
+
+        return length%2 == 0 ?
+                str.substring(0, index - 1) + str.substring(index + 1):
+                str.substring(0,index) + str.substring(index + 1);
+    }
+}
