c08bf737143118ce4c0d0bc1c602c8ab44fccc6b
shankkalra <shankkalra@gmail.com>
2023-04-07 18:03:37 -0300

    Fibbo

--- src/main/java/Revision/General_Problems/Fibbo.java
+++ src/main/java/Revision/General_Problems/Fibbo.java
@@ -10,12 +10,14 @@ public class Fibbo {
         int b = 1;
         int count = 2;
 
+        System.out.println(a);
         while(count<=n){
             int temp=b;
             b=b+a;
             a=temp;
             count++;
+            System.out.println(b);
         }
-        System.out.println(b);
+
     }
 }
