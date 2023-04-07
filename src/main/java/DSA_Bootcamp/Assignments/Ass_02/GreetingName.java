d9f02c33de1ae7030d5061ed4703f7f11eda4227
shankkalra <shankkalra@gmail.com>
2023-04-07 17:47:02 -0300

    Greet

---
+++ src/main/java/DSA_Bootcamp/Assignments/Ass_02/GreetingName.java
@@ -0,0 +1,11 @@
+package DSA_Bootcamp.Assignments.Ass_02;
+
+import java.util.Scanner;
+
+public class GreetingName {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String name = in.next();
+        System.out.println("Hello " + name);
+    }
+}
