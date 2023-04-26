2970b852c80e38cfa0aaca814e90764d3f94cfc4
shankkalra <shankkalra@gmail.com>
2023-04-26 20:18:48 -0300

    Jetbrains

---
+++ src/main/java/Jetbrains/Chuck_Norris_Cipher_Encoder/General/Casting.java
@@ -0,0 +1,31 @@
+package Jetbrains.Chuck_Norris_Cipher_Encoder.General;
+
+public class Casting {
+    public static void main(String[] args) {
+//        float val = 2F;
+//        toString(val);
+
+//        long val = 2L;
+//        toString(val);
+
+//        int val = 2;
+//        toString(val);
+
+//        byte val = 2;
+//        toString(val);
+
+//        char val = 2;
+//        toString(val);
+
+//        double val = 2;
+//        toString(val);
+    }
+
+    public static String toString(long num) {
+        return "";
+    }
+
+    public static String toString(int num) {
+        return "";
+    }
+}
