dd32831aca71e44ed936015106aa14b9b04ee4fa
shankkalra <shankkalra@gmail.com>
2023-04-29 16:08:31 -0300

    Lambda

--- src/main/java/Revision/Strings/StringReverse.java
+++ src/main/java/Revision/Strings/StringReverse.java
@@ -1,26 +1,31 @@
 package Revision.Strings;
 
+import java.util.Scanner;
+
 public class StringReverse {
     public static void main(String[] args) {
-        String str = "ABC";
+        Scanner input = new Scanner(System.in);
+        String str = input.nextLine();
         getReverse(str);
         getReverseWithoutMethod(str);
+        getReverseWithLambda(str);
+    }
+
+    private static void getReverseWithLambda(String str) {
     }
 
     private static void getReverseWithoutMethod(String str) {
-
         char ch[] = str.toCharArray();
-        String nstr="";
+        String newStr ="";
 
-        for (int i = ch.length-1; i >=0; i--) {
-            nstr = nstr+ch[i];
+        for (int i = str.length()-1; i >= 0; i--) {
+            newStr = newStr + ch[i];
         }
-        System.out.println(nstr);
+        System.out.println(newStr);
     }
 
     private static void getReverse(String str) {
         StringBuffer sb = new StringBuffer(str);
-        sb.reverse();
-        System.out.println(sb);
+        System.out.println(sb.reverse());
     }
 }
